b2f58833e1d358d0ad4f0b9248c6363075fbae82
aminaasif20 <aminaasif245@gmail.com>
2025-12-17 21:58:04 +0500

    commit it

--- lab10/task1.c
+++ lab10/task1.c
@@ -7,14 +7,11 @@ sem_t parking_spaces;
 
 void* car(void* arg) {
 int id = *(int*)arg;
-printf("Car %d is trying to park...\n"
-, id);
+printf("Car %d is trying to park...\n", id);
 sem_wait(&parking_spaces); // Try to get a space
-printf("Car %d parked successfully!\n"
-, id);
+printf("Car %d parked successfully!\n", id);
 sleep(2); // Stay parked for 2 seconds
-printf("Car %d is leaving.\n"
-, id);
+printf("Car %d is leaving.\n", id);
 sem_post(&parking_spaces); // Free the space
 return NULL;
 }
@@ -26,12 +23,12 @@ int ids[10];
 sem_init(&parking_spaces, 0, 3);
 // Create 10 cars (more than spaces!)
 for(int i = 0; i < 10; i++) {
-ids[i] = i + 1;
-pthread_create(&cars[i], NULL, car, &ids[i]);
+ ids[i] = i + 1;
+ pthread_create(&cars[i], NULL, car, &ids[i]);
 }
 // Wait for all cars
 for(int i = 0; i < 10; i++) {
-pthread_join(cars[i], NULL);
+ pthread_join(cars[i], NULL);
 }
 sem_destroy(&parking_spaces);
 return 0;
